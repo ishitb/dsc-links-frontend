eb17bfcb41b400396620677c1e27b42c4ba98e11
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2020-08-07 22:15:11 +0530

    Editor added in /editor route

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import './App.css';
 const Navbar = React.lazy(() => import('./components/Navbar/Navbar'));
 const Home = React.lazy(() => import('./pages/Home/Home'));
 const Auth = React.lazy(() => import('./pages/Authentication/Auth'));
+const Editor = React.lazy(() => import('./pages/Editor/Editor'));
 
 function App() {
     return (
         <Suspense fallback={<div></div>}>
             <div id="app">
                 <Navbar />
-                <div id="main-page">
+                <div id="main-page"></div>
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route exact path="/register" component={Auth} />
+                        <Route exact path="/editor" component={Editor} />
                     </Switch>
                 </div>
             </div>
