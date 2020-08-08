3dd90169581ec79ed5411fa3964255b5d93536d3
ishitb <ib370@snu.edu.in>
2020-08-08 15:44:38 +0530

    private route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,27 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const [isAuthenticated, setAuthenticated] = useState(false);
-  if (Cookies.get('token') === 'none' || Cookies.get() == null) {
-    setAuthenticated(false);
-  } else {
-    setAuthenticated(true);
-  }
-  if (isAuthenticated) {
-    console.log('it detects a valid token in the cookies');
-  } else {
-    console.log('there is no valid token in the cookies');
-  }
 
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isAuthenticated ? <Component {...props} /> : <Redirect to="/register" />
-      }
-    />
+  useEffect(() => {
+    if (Cookies.get('token') === 'none' || Cookies.get() == null) {
+      setAuthenticated(false);
+    } else {
+      setAuthenticated(true);
+    }
+  }, []);
+
+  // if (isAuthenticated) {
+  //   console.log('it detects a valid token in the cookies');
+  // } else {
+  //   console.log('there is no valid token in the cookies');
+  // }
+
+  return isAuthenticated ? (
+    <Route {...rest} render={(props) => <Component {...props} />} />
+  ) : (
+    <Redirect to="/register" />
   );
 };
 
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,22 @@
+// import React from 'react';
+// // import PropTypes from 'prop-types';
+
+// const Dashboard = (props) => {
+//   return <div>this should be a protected route</div>;
+// };
+
+// // Dashboard.propTypes = {};
+
+// export default Dashboard;
+
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Dashboard = (props) => {
-  return <div>this should be a protected route</div>;
+const Dashboard = () => {
+  return (
+    <div>
+      <div>this should be a protected route</div>
+    </div>
+  );
 };
-
-Dashboard.propTypes = {};
 
 export default Dashboard;
