7124c9ec28e5ddd17d1fc471a6dc8e90e52832ea
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2020-08-10 00:04:29 +0530

    Changes done in text

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,10 +9,10 @@ const Home = () => {
     return (
             <div> 
             <div id="home">{/* <Carousel />*/ }</div>
-            <div>
-                <h3>SOCIETIES</h3>
-                <h3>RECENT POSTS</h3>
-            </div>
+            <div id="text">
+                <h3 id="text-1">SOCIETIES</h3>
+                <h3 id="text-2">RECENT POSTS</h3>
+            </div>  
             <div id="home">{<Grid />}</div>
             </div>
         );
