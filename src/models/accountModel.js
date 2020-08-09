d01c64ecfa10630fee0d534671db21030c93656f
standmarsh <agrawalshagun07@gmail.com>
2020-08-10 00:56:27 +0530

    nothing significant. Just checking if everything worked.

--- src/models/accountModel.js
+++ src/models/accountModel.js
@@ -1,75 +1,75 @@
-import { thunk, action } from 'easy-peasy';
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import { thunk, action } from "easy-peasy";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export default {
-    // STORE
-    user_logged_in: false,
-    token: null,
-    user_data: {
-        name: 'John Doe',
-    },
+  // STORE
+  user_logged_in: false,
+  token: null,
+  user_data: {
+    name: "John Doe",
+  },
 
-    // THUNKS
-    login_user: thunk(async (actions, { email, password }) => {
-        try {
-            console.log('is this working ? ');
-            const res = await axios.post(
-                '/api/v1/auth/login',
-                { email: email, password: password },
-                {
-                    withCredentials: true,
-                }
-            );
-            await actions.setLoggedIn(res.data['token']);
-        } catch (err) {
-            console.log('this is not working');
+  // THUNKS
+  login_user: thunk(async (actions, { email, password }) => {
+    try {
+      console.log("is this working ? ");
+      const res = await axios.post(
+        "/api/v1/auth/login",
+        { email: email, password: password },
+        {
+          withCredentials: true,
         }
-    }),
+      );
+      await actions.setLoggedIn(res.data["token"]);
+    } catch (err) {
+      console.log("this is not working");
+    }
+  }),
 
-    register_user: thunk(async (actions, { name, email, password }) => {
-        try {
-            console.log('is this working ? ');
-            const res = await axios.post(
-                '/api/v1/auth/register',
-                { email: email, password: password, name: name },
-                {
-                    withCredentials: true,
-                }
-            );
-            actions.setLoggedIn(res.data['token']);
-        } catch (err) {
-            console.log('this is not working');
+  register_user: thunk(async (actions, { name, email, password }) => {
+    try {
+      console.log("is this working ? ");
+      const res = await axios.post(
+        "/api/v1/auth/register",
+        { email: email, password: password, name: name },
+        {
+          withCredentials: true,
         }
-    }),
+      );
+      actions.setLoggedIn(res.data["token"]);
+    } catch (err) {
+      console.log("this is not working");
+    }
+  }),
 
-    logout_user: thunk(async (actions) => {
-        try {
-            console.log('is this working');
-            await axios.get('/api/v1/auth/logout', {
-                withCredentials: true,
-            });
-            actions.setLogout();
-        } catch (err) {
-            console.log('this is not working');
-        }
-    }),
+  logout_user: thunk(async (actions) => {
+    try {
+      console.log("is this working");
+      await axios.get("/api/v1/auth/logout", {
+        withCredentials: true,
+      });
+      actions.setLogout();
+    } catch (err) {
+      console.log("this is not working");
+    }
+  }),
 
-    // ACTIONS
-    setLoggedIn: action(async (state, token) => {
-        Cookies.set('auth-token', token);
-        state.token = token;
-        state.user_logged_in = true;
-        console.log(state.token);
-    }),
+  // ACTIONS
+  setLoggedIn: action(async (state, token) => {
+    Cookies.set("auth-token", token);
+    state.token = token;
+    state.user_logged_in = true;
+    console.log(state.token);
+  }),
 
-    setLogout: action(async (state) => {
-        Cookies.remove('auth-token');
-        state.token = null;
-        state.user_logged_in = false;
-    }),
+  setLogout: action(async (state) => {
+    Cookies.remove("auth-token");
+    state.token = null;
+    state.user_logged_in = false;
+  }),
 
-    store_user_data: action((state, user_data) => {
-        state.user_data = user_data;
-    }),
+  store_user_data: action((state, user_data) => {
+    state.user_data = user_data;
+  }),
 };
--- src/pages/Authentication/Auth.js
+++ src/pages/Authentication/Auth.js
@@ -1,107 +1,101 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useStoreActions } from 'easy-peasy';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useStoreActions } from "easy-peasy";
+import axios from "axios";
 
-import './Auth.css';
+import "./Auth.css";
 
 const Auth = () => {
-    const registerAction = useStoreActions(
-        (actions) => actions.accountModel.register_user
-    );
+  const registerAction = useStoreActions(
+    (actions) => actions.accountModel.register_user
+  );
 
-    const loginAction = useStoreActions(
-        (actions) => actions.accountModel.login_user
-    );
+  const loginAction = useStoreActions(
+    (actions) => actions.accountModel.login_user
+  );
 
-    const logoutAction = useStoreActions(
-        (actions) => actions.accountModel.logout_user
-    );
+  const logoutAction = useStoreActions(
+    (actions) => actions.accountModel.logout_user
+  );
 
-    const login = async () => {
-        loginAction({ email: 'sa831@snu.edu.in', password: '123456' });
-    };
+  const login = async () => {
+    loginAction({ email: "sa831@snu.edu.in", password: "123456" });
+  };
 
-    const logout = async () => {
-        logoutAction();
-    };
+  const logout = async () => {
+    logoutAction();
+  };
 
-    const register = async () => {
-        registerAction({
-            name: 'name',
-            email: 'e@mail.com',
-            password: '1234567890',
-        });
-    };
+  const register = async () => {
+    registerAction({
+      name: "Testing man 2 2 2 2 ",
+      email: "test2222222@mail.com",
+      password: "1234567890",
+    });
+  };
 
-    const [rightPanelActive, setRightPanelActive] = useState(false);
-    return (
-        <div id="auth">
-            <button onClick={login}>Checking if eberything works</button>
-            <button onClick={register}>Checking if register works</button>
-            <Link to="/dashboard"> check</Link>
-            <button onClick={logout}>Checking if logout works</button>
-            <div
-                className={
-                    rightPanelActive
-                        ? 'container right-panel-active'
-                        : 'container'
-                }
-                id="container"
-            >
-                <div className="form-container sign-up-container">
-                    <div className="form">
-                        <h1>Create New Account</h1>
-                        <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <div className="button">Sign Up</div>
-                    </div>
-                </div>
-                <div className="form-container sign-in-container">
-                    <div className="form">
-                        <h1>Sign in</h1>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        {/* <Link to="#">Forgot your password?</Link> */}
-                        <div className="button">Sign In</div>
-                    </div>
-                </div>
-                <div className="overlay-container">
-                    <div className="overlay">
-                        <div className="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>
-                                To keep connected with us please login with your
-                                personal info
-                            </p>
-                            <div
-                                className="ghost button"
-                                id="signIn"
-                                onClick={() => setRightPanelActive(false)}
-                            >
-                                Sign In
-                            </div>
-                        </div>
-                        <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>
-                                Enter your personal details and start journey
-                                with us
-                            </p>
-                            <div
-                                className="ghost button"
-                                id="signUp"
-                                onClick={() => setRightPanelActive(true)}
-                            >
-                                Sign Up
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const [rightPanelActive, setRightPanelActive] = useState(false);
+  return (
+    <div id="auth">
+      <button onClick={login}>Login Check Demo Button</button>
+      <button onClick={register}>Register Check Demo Button</button>
+      <button onClick={logout}>Logout check Button Demo</button>
+      <Link to="/dashboard"> check</Link>
+      <div
+        className={
+          rightPanelActive ? "container right-panel-active" : "container"
+        }
+        id="container"
+      >
+        <div className="form-container sign-up-container">
+          <div className="form">
+            <h1>Create New Account</h1>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <div className="button">Sign Up</div>
+          </div>
         </div>
-    );
+        <div className="form-container sign-in-container">
+          <div className="form">
+            <h1>Sign in</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            {/* <Link to="#">Forgot your password?</Link> */}
+            <div className="button">Sign In</div>
+          </div>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <div
+                className="ghost button"
+                id="signIn"
+                onClick={() => setRightPanelActive(false)}
+              >
+                Sign In
+              </div>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <div
+                className="ghost button"
+                id="signUp"
+                onClick={() => setRightPanelActive(true)}
+              >
+                Sign Up
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
