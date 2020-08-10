a06fd8a2126704cd8f182cdcdf08bde519d4b5f0
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2020-08-10 14:30:39 +0530

    Colors added and grids fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,6 +10,11 @@ const Home = () => {
     return (
         <div id="home">
             <Carousel />
+            <div id="text">
+                <h3 id="text-1">SOCIETIES</h3>
+                <h3 id="text-2">RECENT POSTS</h3>
+            </div>  
+            <Grid/> 
         </div>
     );
 };
