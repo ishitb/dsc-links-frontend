718cb541facd11309ed941782a32aa4cac18acae
standmarsh <agrawalshagun07@gmail.com>
2020-08-17 11:40:46 +0530

    fixing private route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -3,7 +3,9 @@ import { Route, Redirect } from 'react-router-dom';
 import { useStoreState, useStore } from 'easy-peasy';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    const loggedIn = useStore((store) => store.accountModel.user_logged_in);
+    const loggedIn = useStoreState((store) => store.accountModel.user_logged_in);
+    console.log("this is the logged in check inside private route");
+    console.log(loggedIn);
     return loggedIn ? (
         <Route {...rest} render={(props) => <Component {...props} />} />
     ) : (
