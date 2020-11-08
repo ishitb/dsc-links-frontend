9939da0d7e8249b4a32511bb0b1909ac3f4f9c8f
spashii <sameer.pashikanti@gmail.com>
2020-11-09 00:08:55 +0530

    init files

--- src/pages/Home/Carousel.js
+++ src/pages/Home/Carousel.js
@@ -1,45 +1,7 @@
 import React from 'react';
-import { default as BootstrapCarousel } from 'react-bootstrap/Carousel';
 
-import './CarouselCustom.css';
-
-const Carousel = () => {
-    const topPosts = [1, 2, 3, 4, 5];
-
-    // const [index, setIndex] = useState(0);
-
-    return (
-        <div id="carousel">
-            <BootstrapCarousel
-            // activeIndex={index}
-            // onSlide={(index) => setIndex(index)}
-            >
-                {topPosts.map((post, index) => (
-                    <BootstrapCarousel.Item
-                        key={index}
-                        className="d-flex justify-content-center align-content-center"
-                    >
-                        {/* <img
-                            style={{
-                                width: '100%',
-                                height: '100%',
-                                position: 'absolute',
-                                zIndex: '0',
-                            }}
-                            src={orange_bg}
-                            alt="placeholder"
-                        /> */}
-                        {/* <p
-                            className="heading-text dark-fg"
-                            style={{ zIndex: '5' }}
-                        >
-                            {post}
-                        </p> */}
-                    </BootstrapCarousel.Item>
-                ))}
-            </BootstrapCarousel>
-        </div>
-    );
+const Carousel = ({ posts }) => {
+  return <div>hello</div>;
 };
 
 export default Carousel;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,33 +1,37 @@
 import React, { useEffect } from 'react';
 import 'react-bootstrap/Button';
-import { useStoreActions } from 'easy-peasy';
+import { useStoreActions, useStoreState } from 'easy-peasy';
 import './Home.css';
 
 const Carousel = React.lazy(() => import('./Carousel'));
 const Grid = React.lazy(() => import('./Grid'));
 
 const Home = () => {
-    const getAllPosts = useStoreActions(
-        (actions) => actions.postModel.get_all_posts
-    );
+  const getAllPosts = useStoreActions(
+    (actions) => actions.postModel.get_all_posts
+  );
 
-    const getAllPostsFunc = async () => {
-        await getAllPosts();
-    };
+  const getAllPostsFunc = async () => {
+    await getAllPosts();
+  };
 
-    useEffect(() => {
-        getAllPostsFunc();
+  useEffect(() => {
+    getAllPostsFunc();
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <div id="home" className="pb-2">
-            <Carousel />
-            <h3 className="heading-text grey-fg mt-3">SOCIETIES</h3>
-            <Grid />
-        </div>
-    );
+  const allPosts = useStoreState((store) => store.postModel.all_posts);
+
+  console.log(allPosts);
+
+  return (
+    <div id="home" className="pb-2">
+      <Carousel />
+      <h3 className="heading-text grey-fg mt-3">SOCIETIES</h3>
+      <Grid />
+    </div>
+  );
 };
 
 export default Home;
